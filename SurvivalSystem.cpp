4d3c539aeee350240964d93d254e409d830f6340
elder1228 <elderroberto246@gmail.com>
2026-08-16 19:50:36 -0300

    Atualizar o SurvivalSystem.cpp

--- SurvivalSystem.cpp
+++ SurvivalSystem.cpp
@@ -1,4 +1,5 @@
-#include "natives.h"
+#include "generated_offsets.h"
+#include <cstdint>
 
 namespace DeadWastelandXbox 
 {
@@ -7,33 +8,41 @@ namespace DeadWastelandXbox
 
     void UpdateSurvivalSystem() 
     {
-        Ped playerPed = PLAYER::PLAYER_PED_ID();
+        uintptr_t playerPed = *(uintptr_t*)PLAYER_PED_PTR;
 
-        // Consumo de combustível se estiver dirigindo
-        if (PED::IS_PED_IN_ANY_VEHICLE(playerPed, FALSE)) 
+        // Checa se está em veículo via Native
+        bool inVeh = false;
+        InvokeNative(HASH_IS_PED_IN_ANY_VEHICLE, playerPed, FALSE, &inVeh);
+
+        if (inVeh) 
         {
-            Vehicle veh = PED::GET_VEHICLE_PED_IS_IN(playerPed, FALSE);
-            if (VEHICLE::GET_PED_IN_VEHICLE_SEAT(veh, -1) == playerPed) 
+            uintptr_t veh = 0;
+            InvokeNative(HASH_GET_VEHICLE_PED_IS_IN, playerPed, FALSE, &veh);
+
+            uintptr_t driver = 0;
+            InvokeNative(HASH_GET_PED_IN_VEHICLE_SEAT, veh, -1, FALSE, &driver);
+
+            if (driver == playerPed) 
             {
                 g_fuel -= 0.005f;
                 if (g_fuel <= 0.0f) 
                 {
                     g_fuel = 0.0f;
-                    VEHICLE::SET_VEHICLE_ENGINE_ON(veh, FALSE, TRUE, TRUE);
+                    InvokeNative(HASH_SET_VEHICLE_ENGINE_ON, veh, FALSE, TRUE, TRUE);
                 }
             }
         }
 
         // Aplicação do Antídoto Virox (Tecla H ou Atalho Rádio)
-        if (CONTROLS::IS_DISABLED_CONTROL_JUST_PRESSED(0, 74)) 
+        bool pressed = false;
+        InvokeNative(HASH_IS_DISABLED_CONTROL_JUST_PRESSED, 0, 74, &pressed);
+
+        if (pressed && g_isInfected) 
         {
-            if (g_isInfected) 
-            {
-                g_isInfected = false;
-                UI::_SET_NOTIFICATION_TEXT_ENTRY("STRING");
-                UI::_ADD_TEXT_COMPONENT_STRING("~g~[VIROX] Infecção curada com sucesso!");
-                UI::_DRAW_NOTIFICATION(FALSE, TRUE);
-            }
+            g_isInfected = false;
+            InvokeNative(HASH_SET_NOTIFICATION_TEXT_ENTRY, "STRING");
+            InvokeNative(HASH_ADD_TEXT_COMPONENT_STRING, "~g~[VIROX] Infecção curada com sucesso!");
+            InvokeNative(HASH_DRAW_NOTIFICATION, FALSE, TRUE);
         }
     }
 }
