5be739755e7077dee11800100318c4f437e5ffa3
elder1228 <elderroberto246@gmail.com>
2026-08-16 19:54:35 -0300

    Atualizar o AirdropSystem.cpp

--- AirdropSystem.cpp
+++ AirdropSystem.cpp
@@ -1,49 +1,56 @@
-#include "natives.h"
-#include "types.h"
+#include "generated_offsets.h"
+#include <cstdint>
+#include <cstdlib>
 
 namespace DeadWastelandXbox 
 {
+    struct Vector3 { float x, y, z; };
+
     Vector3 g_lastAirdropPos;
     bool g_airdropActive = false;
 
     void SpawnAirdrop() 
     {
-        Ped playerPed = PLAYER::PLAYER_PED_ID();
-        Vector3 pCoords = ENTITY::GET_ENTITY_COORDS(playerPed, TRUE);
+        uintptr_t playerPed = *(uintptr_t*)PLAYER_PED_PTR;
+        Vector3 pCoords;
+        InvokeNative(HASH_GET_ENTITY_COORDS, playerPed, TRUE, &pCoords);
 
-        // Posição de queda (raio de 100m a 200m do jogador, 150m de altura)
-        float offsetX = (float)(GAMEPLAY::GET_RANDOM_INT_IN_RANGE(100, 200));
-        float offsetY = (float)(GAMEPLAY::GET_RANDOM_INT_IN_RANGE(100, 200));
+        float offsetX = (float)(rand() % 100 + 100);
+        float offsetY = (float)(rand() % 100 + 100);
         g_lastAirdropPos = { pCoords.x + offsetX, pCoords.y + offsetY, pCoords.z + 150.0f };
 
-        // Spawna caixa de suprimentos da Merryweather
-        Hash crateHash = GAMEPLAY::GET_HASH_KEY("prop_box_wood02a");
-        STREAMING::REQUEST_MODEL(crateHash);
-        while (!STREAMING::HAS_MODEL_LOADED(crateHash)) WAIT(0);
-
-        Object crate = OBJECT::CREATE_OBJECT(crateHash, g_lastAirdropPos.x, g_lastAirdropPos.y, g_lastAirdropPos.z, TRUE, TRUE, FALSE);
-        ENTITY::SET_ENTITY_DYNAMIC(crate, TRUE);
+        uint32_t crateHash = 0;
+        InvokeNative(HASH_GET_HASH_KEY, "prop_box_wood02a", &crateHash);
+        InvokeNative(HASH_REQUEST_MODEL, crateHash);
         
-        // Aplica paraquedas/fumaça de sinalização
-        GRAPHICS::_ADD_PETROL_DECAL(g_lastAirdropPos.x, g_lastAirdropPos.y, g_lastAirdropPos.z, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
-        
-        Blip crateBlip = UI::ADD_BLIP_FOR_ENTITY(crate);
-        UI::SET_BLIP_SPRITE(crateBlip, 306); // Ícone de Caixa
-        UI::SET_BLIP_COLOUR(crateBlip, 2);  // Verde
+        bool loaded = false;
+        while (!loaded) {
+            InvokeNative(HASH_HAS_MODEL_LOADED, crateHash, &loaded);
+            Sleep(0);
+        }
 
-        UI::_SET_NOTIFICATION_TEXT_ENTRY("STRING");
-        UI::_ADD_TEXT_COMPONENT_STRING("~g~[AIRDROP] Suprimentos da Merryweather a caminho!");
-        UI::_DRAW_NOTIFICATION(FALSE, TRUE);
+        uintptr_t crate = 0;
+        InvokeNative(HASH_CREATE_OBJECT, crateHash, g_lastAirdropPos.x, g_lastAirdropPos.y, g_lastAirdropPos.z, TRUE, TRUE, FALSE, &crate);
+        InvokeNative(HASH_SET_ENTITY_DYNAMIC, crate, TRUE);
+        
+        InvokeNative(HASH_ADD_PETROL_DECAL, g_lastAirdropPos.x, g_lastAirdropPos.y, g_lastAirdropPos.z, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f);
+        
+        uintptr_t crateBlip = 0;
+        InvokeNative(HASH_ADD_BLIP_FOR_ENTITY, crate, &crateBlip);
+        InvokeNative(HASH_SET_BLIP_SPRITE, crateBlip, 306);
+        InvokeNative(HASH_SET_BLIP_COLOUR, crateBlip, 2);
+
+        InvokeNative(HASH_SET_NOTIFICATION_TEXT_ENTRY, "STRING");
+        InvokeNative(HASH_ADD_TEXT_COMPONENT_STRING, "~g~[AIRDROP] Suprimentos da Merryweather a caminho!");
+        InvokeNative(HASH_DRAW_NOTIFICATION, FALSE, TRUE);
 
         g_airdropActive = true;
     }
 
     void UpdateAirdropSystem() 
     {
-        // Tecla 9 no Teclado USB para solicitar Airdrop de Emergência
-        if (CONTROLS::IS_DISABLED_CONTROL_JUST_PRESSED(0, 118)) 
-        {
-            SpawnAirdrop();
-        }
+        bool pressed = false;
+        InvokeNative(HASH_IS_DISABLED_CONTROL_JUST_PRESSED, 0, 118, &pressed);
+        if (pressed) SpawnAirdrop();
     }
 }
